0af4ad8e2f8df7d520d5b5a07c2515c33dd91854
Hesnan Ávila <avilahesnan@outlook.com>
2024-04-17 16:46:12 -0300

    config: add vuex

---
+++ src/components/ContadorComponent.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h2 class="font-weight-light">Contador: {{ $store.state.contador }}</h2>
+        <button
+            class="btn btn-lg btn-danger me-1"
+            @click="decrementar">
+                -
+        </button>
+        <button
+            class="btn btn-lg btn-success"
+            @click="incrementar">
+                +
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        decrementar () {
+            this.$store.state.contador--
+        },
+        incrementar () {
+            this.$store.state.contador++
+        }
+    }
+}
+</script>
